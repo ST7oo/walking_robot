0d37f5fea132be4a1d966ad476ff7a0978642079
Rodney <reln13st@gmail.com>
2016-09-10 22:03:11 +0200

    calculate policy and steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -6,9 +6,9 @@ import {Component} from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   discount_factor: number = 0.5;
-  static transitions = [[1, 3, 4, 12], //0
+  transitions = [[1, 3, 4, 12], //0
     [0, 2, 5, 13], //1
     [3, 1, 6, 14], //2
     [2, 0, 7, 15], //3
@@ -25,21 +25,95 @@ export class AppComponent {
     [15, 13, 10, 2], //14
     [14, 12, 11, 3]]; //15
   rewards = [[0, -10, 0, -10], //0
-    [0, 0, -10, 0], //1
-    [0, 0, -10, 0], //2
+    [0, 0, -10, -10], //1
+    [0, 0, -10, -10], //2
     [0, -10, 0, -10], //3
     [-10, -10, 0, 0], //4
-    [-10, -10, -10, -10], //5
-    [-10, -10, -10, -10], //6
+    [-5, -10, -5, -10], //5
+    [-5, -10, -5, -10], //6
     [-10, 10, 0, 0], //7
     [-10, -10, 0, 0], //8
-    [-10, -10, -10, -10], //9
-    [-10, -10, -10, -10], //10
+    [-5, -10, -5, -10], //9
+    [-5, -10, -5, -10], //10
     [-10, 10, 0, 0], //11
     [0, -10, 0, -10], //12
     [0, 0, -10, 10], //13
     [0, 0, -10, 10], //14
     [0, -10, 0, -10]]; //15
-  v = [];
-  policy = [];
+
+  policy = this.calculate_policy(this.transitions, this.rewards, this.discount_factor);
+  initial_state;
+  num_steps = 20;
+  steps = [];
+
+  ngOnInit(){
+    //this.simulate();
+  }
+
+  simulate(random: boolean){
+    if (random) {
+      this.initial_state = Math.floor(Math.random() * (15 - 0 + 1)) + 0;
+    }
+    this.steps = this.getSteps(this.policy, this.transitions, this.initial_state, this.num_steps);
+  }
+
+  getSteps(policy: number[], transitions: number[][], initial_state: number, num_steps: number){
+    let steps = [initial_state];
+    let state = initial_state;
+    for (let i =1; i<num_steps; i++){
+      let new_state = transitions[state][policy[state]];
+      steps.push(new_state);
+      state = new_state;
+    }
+    return steps;
+  }
+
+  calculate_policy(transitions: number[][], rewards: number[][], discount_factor: number) {
+    // let V = rewards.map(x => x[0]);
+    let V = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let policy = [];
+    let new_policy = this.argmax(rewards, discount_factor, V, transitions);
+    let updated = true;
+    let i = 0;
+    while (updated && i < 100) {
+      policy = new_policy;
+      for (let s in V) {
+        V[s] = rewards[s][policy[s]] + discount_factor * V[transitions[s][policy[s]]];
+      }
+      new_policy = this.argmax(rewards, discount_factor, V, transitions);
+      console.log(policy);
+      if (this.arraysEqual(new_policy, policy)) {
+        updated = false;
+      }
+      i++;
+    }
+    return new_policy;
+  }
+
+  argmax(r: number[][], gamma: number, v: number[], delta: number[][]) {
+    let a_max = 0,
+      pi = [],
+      max;
+    for (let s in r) {
+      for (let a in r[s]) {
+        let tmp = r[s][a] + gamma * v[delta[s][a]];
+        if (!max || tmp > max) {
+          max = tmp;
+          a_max = parseInt(a);
+        }
+      }
+      pi[s] = a_max;
+    }
+    return pi;
+  }
+
+  arraysEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length)
+      return false;
+    for (var i = arr1.length; i--;) {
+      if (arr1[i] !== arr2[i])
+        return false;
+    }
+    return true;
+  }
 }
