cbe0a92f2fed7798ee801504b8488eb134a94ec1
Rodney <reln13st@gmail.com>
2016-09-14 21:11:49 +0200

    panel parameters

--- app/app.component.ts
+++ app/app.component.ts
@@ -13,6 +13,10 @@ export class AppComponent {
   discount_factor: number = 0.5;
 	learning_rate: number = 0.1;
 	exploration_rate: number = 0.2;
+	states = [[0, 1, 2, 3],
+		[4, 5, 6, 7],
+		[8, 9, 10, 11],
+		[12, 13, 14, 15]];
   transitions = [[1, 3, 4, 12], //0
     [0, 2, 5, 13], //1
     [3, 1, 6, 14], //2
@@ -50,17 +54,20 @@ export class AppComponent {
   calculatingQ = true;
   calculatingQlearning = true;
   calculatingQlearning_greedy = true;
-	show_animation_parameters = true;
-	show_policies_parameters = false;
-  initial_state: number = 0;
+	show_parameters = [true, false, false];
+  initial_state = 0;
+  num_steps = 20;
+	speed = 800;
+	T = 10000;
+	max_iterations = 200;
+	random_max = 10;
+	iterations_V: number;
+	iterations_Q: number;
   policyV: number[] = [];
   policyQ: number[] = [];
   policyQlearning: number[] = [];
   policyQlearning_greedy: number[] = [];
 	best_policy = [0, 1, 0, 2, 3, 2, 0, 1, 2, 2, 0, 1, 0, 3, 3, 0];
-  num_steps = 20;
-	speed = 800;
-	T = 10000;
   steps: Array<number>[] = Array(this.num_policies);
 	animations: any[] = Array(this.num_policies);
 	current_steps: number[] = Array(this.num_policies);
@@ -73,10 +80,10 @@ export class AppComponent {
     setTimeout(() => {
       this.calculate_policies();
     }, 1000);
-		for (let i = 0; i < this.num_states; i++) {
-			let image = new Image();
-			image.src = `app/img/step${i}.png`;
-		}
+		// for (let i = 0; i < this.num_states; i++) {
+		// 	let image = new Image();
+		// 	image.src = `app/img/step${i}.png`;
+		// }
   }
 
 	calculate_policies() {
@@ -87,13 +94,13 @@ export class AppComponent {
 		for (let i = 0; i < this.num_policies; i++) {
 			this.reset_animation(i);
 		}
-		this.policyV = this.calculate_policyV(this.transitions, this.rewards, this.discount_factor);
+		[this.policyV, this.iterations_V] = this.calculate_policyV(this.transitions, this.rewards, this.discount_factor, this.num_states, this.num_actions, this.max_iterations);
 		this.calculatingV = false;
-		this.policyQ = this.calculate_policyQ(this.transitions, this.rewards, this.discount_factor);
+		[this.policyQ, this.iterations_Q] = this.calculate_policyQ(this.transitions, this.rewards, this.discount_factor, this.num_states, this.num_actions, this.max_iterations, this.random_max);
 		this.calculatingQ = false;
-		this.policyQlearning = this.calculate_Qlearning(math.randomInt(this.num_states - 1), this.T, this.exploration_rate, this.learning_rate, this.discount_factor);
+		this.policyQlearning = this.calculate_Qlearning(math.randomInt(this.num_states - 1), this.T, this.exploration_rate, this.learning_rate, this.discount_factor, this.num_states, this.num_actions, this.random_max);
 		this.calculatingQlearning = false;
-		this.policyQlearning_greedy = this.calculate_Qlearning(math.randomInt(this.num_states - 1), this.T, this.exploration_rate, this.learning_rate, this.discount_factor, true);
+		this.policyQlearning_greedy = this.calculate_Qlearning(math.randomInt(this.num_states - 1), this.T, this.exploration_rate, this.learning_rate, this.discount_factor, this.num_states, this.num_actions, this.random_max, true);
 		this.calculatingQlearning_greedy = false;
 	}
 
@@ -153,12 +160,8 @@ export class AppComponent {
 		this.initial_state = math.randomInt(this.num_states - 1);
 	}
 
-	toggle_animation_parameters() {
-		this.show_animation_parameters = !this.show_animation_parameters;
-	}
-
-	toggle_policies_parameters() {
-		this.show_policies_parameters = !this.show_policies_parameters;
+	toggle_parameters(param: number) {
+		this.show_parameters[param] = !this.show_parameters[param];
 	}
 
   getSteps(policy: number[], transitions: number[][], initial_state: number, num_steps: number) {
@@ -172,15 +175,15 @@ export class AppComponent {
     return steps;
   }
 
-  calculate_policyV(transitions: number[][], rewards: number[][], discount_factor: number) {
-    let V = math.randomInt([this.num_states], this.num_actions - 1);
-    let policy: number[] = Array(this.num_states);
-    let new_policy: number[] = Array(this.num_states);
+  calculate_policyV(transitions: number[][], rewards: number[][], discount_factor: number, num_states: number, num_actions: number, max_iterations: number): [number[], number] {
+    let V = math.randomInt([num_states], num_actions - 1);
+    let policy: number[] = Array(num_states);
+    let new_policy: number[] = Array(num_states);
 		for (let s in rewards) {
 			new_policy[s] = this.argmax(rewards[s], (a: number) => rewards[s][a] + discount_factor * V[transitions[s][a]]);
 		}
     let i = 0;
-    while (!math.deepEqual(new_policy, policy) && i < 100) {
+    while (!math.deepEqual(new_policy, policy) && i < max_iterations) {
       policy = math.clone(new_policy);
       for (let s in V) {
         V[s] = rewards[s][policy[s]] + discount_factor * V[transitions[s][policy[s]]];
@@ -188,19 +191,18 @@ export class AppComponent {
       }
       i++;
     }
-		console.log('V:', i);
-    return new_policy;
+    return [new_policy, i];
   }
 
-  calculate_policyQ(transitions: number[][], rewards: number[][], discount_factor: number) {
-    let Q = math.randomInt([this.num_states, this.num_actions], 10);
+  calculate_policyQ(transitions: number[][], rewards: number[][], discount_factor: number, num_states: number, num_actions: number, max_iterations: number, random_max: number): [number[], number] {
+    let Q = math.randomInt([num_states, num_actions], random_max);
     let policy: number[] = [];
-    let new_policy: number[] = Array(this.num_states);
+    let new_policy: number[] = Array(num_states);
 		for (let s in Q) {
 			new_policy[s] = this.argmax(Q[s], (a: number) => Q[s][a]);
 		}
     let i = 0;
-    while (!math.deepEqual(new_policy, policy) && i < 100) {
+    while (!math.deepEqual(new_policy, policy) && i < max_iterations) {
       policy = math.clone(new_policy);
       for (let s in Q) {
         for (let a in Q[s]) {
@@ -210,19 +212,18 @@ export class AppComponent {
       }
       i++;
     }
-		console.log('Q:', i);
-    return new_policy;
+    return [new_policy, i];
   }
 
-	calculate_Qlearning(initial_state: number, T: number, exploration_rate: number, learning_rate: number, discount_factor: number, greedy = false) {
+	calculate_Qlearning(initial_state: number, T: number, exploration_rate: number, learning_rate: number, discount_factor: number, num_states: number, num_actions: number, random_max: number, greedy = false) {
 		let action: number, new_state: number, reward: number;
-		let policy = Array(this.num_states);
+		let policy = Array(num_states);
 		let epsilon = exploration_rate;
-		let Q = math.randomInt([this.num_states, this.num_actions], 10);
+		let Q = math.randomInt([num_states, num_actions], random_max);
 		let state = initial_state;
 		for (let t = 0; t < T; t++) {
 			if (!greedy && math.random() < epsilon) {
-				action = math.randomInt(this.num_actions - 1);
+				action = math.randomInt(num_actions - 1);
 			} else {
 				action = Q[state].indexOf(math.max(Q[state]));
 			}
