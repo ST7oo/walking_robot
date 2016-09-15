c49b3732df45cd971d4b0c201eb8d32ecca559a8
Rodney <reln13st@gmail.com>
2016-09-15 12:45:04 +0200

    changed to angular2-express
    testing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
+
 
 import { AppComponent }  from './app.component';
+import { AboutComponent } from "./components/about/about.component";
+import { routing } from "./routes";
+import { HomeComponent } from "./components/home/home.component";
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule,
+        HttpModule,
+        JsonpModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        AboutComponent,
+        HomeComponent
+    ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
